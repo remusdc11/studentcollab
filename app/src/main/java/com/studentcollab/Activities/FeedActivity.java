6138337441f80029b720753b07c208873ea6c17e
remus.dascalu@byteunit.com <remus.dascalu@byteunit.com>
2021-04-07 14:43:50 +0300

    Icons. Logout. Feed bottom navigation

--- app/src/main/java/com/studentcollab/Activities/FeedActivity.java
+++ app/src/main/java/com/studentcollab/Activities/FeedActivity.java
@@ -2,22 +2,55 @@ package com.studentcollab.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.studentcollab.Globals.LoadingDialog;
+import com.studentcollab.Globals.Variables;
 import com.studentcollab.R;
 
 public class FeedActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private LoadingDialog loadingDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
 
-        View rootLayout = findViewById(R.id.root_layout);
+        //View rootLayout = findViewById(R.id.root_layout);
 
-        Snackbar.make(rootLayout, R.string.feed_activity_welcome, Snackbar.LENGTH_LONG).show();
+        View container = findViewById(R.id.container);
+
+        Snackbar.make(container, R.string.feed_activity_welcome, Snackbar.LENGTH_LONG).show();
+
+        mAuth = FirebaseAuth.getInstance();
+        loadingDialog = new LoadingDialog(FeedActivity.this);
+
+
+        View logOut = findViewById(R.id.button_log_out);
+
+        logOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadingDialog.start();
+                mAuth.signOut();
+                Variables.user = null;
+                Intent intent = new Intent(FeedActivity.this, LoginActivity.class);
+                loadingDialog.dismiss();
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+
 
     }
 }
